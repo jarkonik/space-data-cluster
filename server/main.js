d040de9a5b4e75069682c60e6dd0a189cae51480
Jaroslaw Konik <konikjar@gmail.com>
2016-04-02 21:14:25 +0200

    Serve static assets through express

--- server/main.js
+++ server/main.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.use(express.static(__dirname + '/../client'));
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
