afec204238badbd699388614cf0cc915f410f683
Jaroslaw Konik <konikjar@gmail.com>
2016-04-08 03:13:24 +0200

    Cleanup

--- server/main.js
+++ server/main.js
@@ -1,12 +1,53 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/api', function (req, res) {
+const express = require('express');
+const expressApp = express();
+const path = require('path');
+
+expressApp.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use(express.static(__dirname + '/../public'));
+expressApp.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+expressApp.listen(3000);
+
+const app = require('app');  // Module to control application life.
+const BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('http://localhost:3000/index.html');
+
+  // Open the DevTools.
+  mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 });
